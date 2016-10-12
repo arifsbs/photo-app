34e52d4a7d9fd25f695946838950ff2916c59545
Arif Shaikh <SYSDEVA5@morrisonsplc.co.uk>
2016-10-12 15:27:40 +0100

    Added logic to make http call to photo service

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,9 @@ var nameSpace = angular.module("ImageApp", []);
 
 nameSpace.controller("imageFunction", ['$scope','$http', function($scope, $http)
 		{    
+			$http.get('http://localhost:8080/photo-album').success(function (response) {
+		                $scope.images = response;
+			});
 
 		}]
 );
